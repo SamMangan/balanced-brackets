3b1ad7d730d056ce72aac3d6b7ac64bc6aff16c5
Sam Mangan <110870725+SamMangan@users.noreply.github.com>
2022-08-09 01:42:21 +0000

    Make indenting consistent

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from solutions import string_solution, recursive_solution, stack_solution
 from balance import balance
 
 def main():
-    run_tests(string_solution)
-    run_tests(stack_solution)
-    run_tests(recursive_solution)
+  run_tests(string_solution)
+  run_tests(stack_solution)
+  run_tests(recursive_solution)
 
-    # Interactive Demo
-    line = input("Enter an expression to validate: ")
-    is_valid = stack_solution(line)
-    print(f"{line} is {'' if is_valid else 'not'} well formatted!")
+  # Interactive Demo
+  line = input("Enter an expression to validate: ")
+  is_valid = stack_solution(line)
+  print(f"{line} is {'' if is_valid else 'not'} well formatted!")
 
-    # Warmup excercise to complete valid expressions
-    # e.g. "[{<" gives ">}]"
-    line = input("Enter a line with brackets that need closing: ")
-    balance(line)
+  # Warmup excercise to complete valid expressions
+  # e.g. "[{<" gives ">}]"
+  line = input("Enter a line with brackets that need closing: ")
+  balance(line)
 
 main()
--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
 def run_tests(test_subject):
-    assert test_subject("()"), "A single matching pair is balanced"
-    assert test_subject("([{<>}])"), "Nested matching pairs are balanced"
-    assert test_subject("()[]{}"), "Consecutive matching pairs are balanced"
-    assert test_subject("5*(1+4)"), "Maths Expression is balanced"
+  assert test_subject("()"), "A single matching pair is balanced"
+  assert test_subject("([{<>}])"), "Nested matching pairs are balanced"
+  assert test_subject("()[]{}"), "Consecutive matching pairs are balanced"
+  assert test_subject("5*(1+4)"), "Maths Expression is balanced"
 
-    assert not test_subject("("), "A single bracket is unbalanced"
-    assert not test_subject("(]"), "A mismatched pair is unbalanced"
-    assert not test_subject("([)]"), "Interleaving brackets are unbalanced"
-    assert not test_subject(
-        ")("), "An 'inside-out' matching pair is unbalanced"
-    assert not test_subject("5*(1+4"), "Maths Expression is unbalanced"
+  assert not test_subject("("), "A single bracket is unbalanced"
+  assert not test_subject("(]"), "A mismatched pair is unbalanced"
+  assert not test_subject("([)]"), "Interleaving brackets are unbalanced"
+  assert not test_subject(
+      ")("), "An 'inside-out' matching pair is unbalanced"
+  assert not test_subject("5*(1+4"), "Maths Expression is unbalanced"
 
-    print(f"[{test_subject.__name__}] Tests passed!")
+  print(f"[{test_subject.__name__}] Tests passed!")
