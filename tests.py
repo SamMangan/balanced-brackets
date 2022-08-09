954af8b40d100f1bf6767f6405b17c5fb8add14a
Sam Mangan <110870725+SamMangan@users.noreply.github.com>
2022-08-09 02:23:01 +0000

    add some failing balance tests

--- tests.py
+++ tests.py
@@ -15,7 +15,18 @@ def run_is_balanced_tests(test_subject):
 
   
 def run_balance_tests(test_subject):
+  # trivial cases
+  assert test_subject("") == "", "Balance an empty string"
+  assert test_subject("5") == "5", "Balance an expression without brackets"
+
+  # opening brackets only
   assert test_subject("(") == "()", "Balance a single opening bracket"
-  assert test_subject("<[(") == "<[()]>", "Balance a sequence of opening brackets"
+  assert test_subject("<{[(") == "<{[()]}>", "Balance a sequence of opening brackets"
+  assert test_subject("(2+[4x3") == "(2+[4x3])", "Balance Maths Expression starting with bracket"
+  assert test_subject("5-(2+[4x3") == "5-(2+[4x3])", "Balance Maths Expression starting with non-bracket"
+
+  # openening and closing brackets
+  assert test_subject("[{}]<(") == "[{}]<()>", "Balance line starting with an already-balanced expression"
+  assert test_subject("([{}]<(") == "([{}]<()>)", "Balance line containing an already-balanced expression"
 
   print(f"[{test_subject.__name__}] Tests passed!")
